0d7d65ca6326000c361f0c2a624d944c129155bc
TrionMma <88440548+TrionMma@users.noreply.github.com>
2021-08-04 10:42:01 -0400

    Inital Commit of Hellow World Test

---
+++ WinHelloWorld/WinHelloWorld/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace WinHelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bnClose
+            // 
+            this.bnClose.Location = new System.Drawing.Point(350, 307);
+            this.bnClose.Name = "bnClose";
+            this.bnClose.Size = new System.Drawing.Size(75, 23);
+            this.bnClose.TabIndex = 0;
+            this.bnClose.Text = "Close";
+            this.bnClose.UseVisualStyleBackColor = true;
+            this.bnClose.Click += new System.EventHandler(this.bnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(215, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HELLO WORLD!!!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bnClose);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WinHelloWorld";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnClose;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
